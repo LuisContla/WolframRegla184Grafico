0b502e44f3d755ff17951cde8a94d4a279ba062c
Luis Contla <luis.contla.mota30@gmail.com>
2025-05-13 19:15:00 -0600

    App Creada
    Se creo la app base de React usando Vite

---
+++ public/Pages/WolframGraphic.jsx
@@ -0,0 +1,14 @@
+import "../../src/assets/script.js";
+
+function WolframGraphic() {
+  return (
+    <>
+      <div className="contenedor-principal">
+        Wolfram Regla 184
+      </div>
+    </>
+
+  )
+}
+
+export default WolframGraphic
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './assets/App.css'
+
+import WolframGraphic from "../public/Pages/WolframGraphic";
+
+function App() {
+
+  return (
+    <>
+      <WolframGraphic />
+    </>
+  )
+}
+
+export default App
